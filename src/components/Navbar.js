95548ea1b02c46b6ca7c385aae9bd5dc1aa7a008
ShahriarSaleque <salequeshahriar@gmail.com>
2019-11-15 16:24:22 +0600

    made navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,43 @@
+import React from "react";
+import alarm from "../img/alarm.png";
+import message from "../img/envelope.png";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-sm navbar-light bg-light">
+      <div className="container">
+        <a className="navbar-brand" href="#">
+          Calorie<span className="text-primary">Tracker</span>
+        </a>
+
+        <ul className="navbar-nav">
+          <li className="nav-item mx-3">
+            <a className="nav-link" href="#">
+              Name
+            </a>
+          </li>
+          <li className="nav-item mx-3">
+            <a className="nav-link" href="#">
+              <img
+                src={alarm}
+                alt="alarm"
+                style={{ width: "20px", height: "20px" }}
+              />
+            </a>
+          </li>
+          <li className="nav-item mx-3">
+            <a className="nav-link" href="#">
+              <img
+                src={message}
+                alt="message"
+                style={{ width: "20px", height: "20px" }}
+              />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
